89261e9f95e7ef71732f74d33daa54fde005a3cf
donovan-cartier <donovan.cartier0@gmail.com>
2026-01-30 22:50:26 +0100

    added player run

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const RUN_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 
 @export var camera: Camera3D
@@ -14,7 +15,6 @@ func _input(event):
 		get_tree().quit()
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -22,15 +22,20 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	var calculated_speed: float = _calculate_speed()
+
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * calculated_speed
+		velocity.z = direction.z * calculated_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, calculated_speed)
+		velocity.z = move_toward(velocity.z, 0, calculated_speed)
 
 	move_and_slide()
+
+func _calculate_speed() -> float:
+	if Input.is_action_pressed("run"):
+		return RUN_SPEED
+	return SPEED
