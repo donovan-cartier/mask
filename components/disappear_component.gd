5371ceaf0387a2cb7e204e67e2b507eee01124a9
donovan-cartier <donovan.cartier0@gmail.com>
2026-01-31 16:09:46 +0100

    added fence

--- components/disappear_component.gd
+++ components/disappear_component.gd
@@ -7,9 +7,9 @@ enum TimePeriod {
 	FUTURE
 }
 
-# TODO: faire en fonction du time period
 @export var time_period: TimePeriod = TimePeriod.PRESENT
 @export var custom_owner: Node3D
+@export var invert: bool = false
 
 signal disappeared
 signal reappeared
@@ -20,7 +20,7 @@ func _ready():
 	assert(owner is StaticBody3D, "Faut que le owner soit un StaticBody3D mon bro")
 	Nodes.player.changed_time_period.connect(_on_time_period_changed)
 
-	if Nodes.player.current_time_period == time_period:
+	if Nodes.player.current_time_period == time_period && !invert:
 		reappear()
 	else:
 		disappear()
@@ -38,7 +38,13 @@ func reappear():
 	reappeared.emit()
 
 func _on_time_period_changed(new_time_period: TimePeriod) -> void:
-	if new_time_period == time_period:
-		reappear()
+	if invert:
+		if new_time_period != time_period:
+			reappear()
+		else:
+			disappear()
 	else:
-		disappear()
+		if new_time_period == time_period:
+			reappear()
+		else:
+			disappear()
