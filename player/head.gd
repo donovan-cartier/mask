783d9efd3668ea3b81b749a0fc504a3a2cc8f3e8
donovan-cartier <donovan.cartier0@gmail.com>
2026-01-31 17:38:52 +0100

    added fps rig

--- player/head.gd
+++ player/head.gd
@@ -2,8 +2,20 @@ extends Node3D
 
 var mouse_sense = 0.1
 
+@onready var hands = %Hands
+@onready var default_hand_position = hands.position
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		owner.rotate_y(deg_to_rad(-event.relative.x * mouse_sense))
 		rotate_x(deg_to_rad(-event.relative.y * mouse_sense))
 		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		sway(event.relative)
+
+func _process(delta):
+	hands.position.x = lerp(hands.position.x, default_hand_position.x, delta * 5)
+	hands.position.y = lerp(hands.position.y, default_hand_position.y, delta * 5)
+
+func sway(sway_amount: Vector2) -> void:
+	hands.position.x -= sway_amount.x * 0.0001
+	hands.position.y += sway_amount.y * 0.0001
