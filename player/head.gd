2f973b1486910abc925932b30f8cec2bc52e4814
donovan-cartier <donovan.cartier0@gmail.com>
2026-01-31 18:06:20 +0100

    screen flicker

--- player/head.gd
+++ player/head.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 var mouse_sense = 0.1
+var flicker_timer = 0.0
+var flicker_delay = 0.5  # Délai initial
 
 @onready var hands = %Hands
 @onready var default_hand_position = hands.position
+@onready var m_screen = load("res://player/shifter/m_screen.tres")
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -15,6 +18,14 @@ func _input(event):
 func _process(delta):
 	hands.position.x = lerp(hands.position.x, default_hand_position.x, delta * 5)
 	hands.position.y = lerp(hands.position.y, default_hand_position.y, delta * 5)
+	flicker_timer -= delta
+	if flicker_timer <= 0:
+		random_flicker()
+		flicker_timer = randf_range(0.1, .2)  # Délai aléatoire entre 0.1 et 1.0 secondes
+
+func random_flicker() -> void:
+	var flicker_value = randf_range(0, 1.93)  # Valeur aléatoire entre 0 et 1
+	m_screen.set("emission_energy_multiplier", flicker_value)
 
 func sway(sway_amount: Vector2) -> void:
 	hands.position.x -= sway_amount.x * 0.0001
